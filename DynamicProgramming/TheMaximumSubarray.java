1b4941432e16ab590b5f8294f0415dfa15a08669
Ming Yang <mgy209@nyu.edu>
2016-10-25 20:58:15 -0400

    Solved maximum subarray problem

---
+++ DynamicProgramming/TheMaximumSubarray.java
@@ -0,0 +1,56 @@
+/*
+Given an array A={a1,a2,...,an} of N elements, find the maximum
+possible sum of a:
+1. Contiguous subarray
+2. Non-contiguous (not necessarily contiguous) subarray.
+
+Empty subarrays/subsequences should not be considered.
+*/
+
+import java.io.*;
+import java.util.*;
+import java.lang.Math;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int numTests = input.nextInt();
+        for (int i = 0; i < numTests; i++) {
+            int arrSize = input.nextInt();
+            int[] arr = new int[arrSize];
+
+            for (int j = 0; j < arrSize; j++) {
+                arr[j] = input.nextInt();
+            }
+
+            System.out.println(maxContig(arr) + " " + maxNotContig(arr));
+        }
+    }
+
+    private static int maxContig(int[] arr) {
+        int maxEndingHere = arr[0];
+        int maxSoFar = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+
+        return maxSoFar;
+    }
+
+    private static int maxNotContig(int[] arr) {
+        int max = arr[0];
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) sum += arr[i];
+            if (arr[i] > max) max = arr[i];
+        }
+
+        if (sum == 0)   return max;
+        else            return sum;
+    }
+}
